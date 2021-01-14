ffd6c877fd4aa1901536faa674b810415341be03
chaloxc <gonzalo.baldassarre@gmail.com>
2021-01-14 10:50:06 -0300

    Agrego outarguments log

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -1,3 +1,5 @@
+const { alert } = require("window-or-global");
+
 define([
     'postmonger'
 ], function (
@@ -122,8 +124,7 @@ define([
         let urlRedirect = document.getElementById("urlRedirect").value ? document.getElementById("urlRedirect").value : "";
         let isChecked = document.getElementById("redirect").checked;
         let args = [{ ...dataObject, WPNtitle, WPNmessage, urlRedirect, isChecked }];   
-        console.log(payload['arguments'].execute && payload['arguments'].execute.outArguments ? "@@@ Out Arguments: " : "@@@ No tiene out arguments"); 
-        console.log("@@@ Payload "+payload['arguments']);
+        alert(payload['arguments'].execute && payload['arguments'].execute.outArguments ? "@@@ Out Arguments: " : "@@@ No tiene out arguments"); 
         payload['arguments'].execute.inArguments = args;
         payload['metaData'].isConfigured = true;
         connection.trigger('updateActivity', payload);  
